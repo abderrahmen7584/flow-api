e53fb76c6798969855b780a5e70657d49bb8fd81
abderrahmen7584 <157791567+abderrahmen7584@users.noreply.github.com>
2025-12-07 16:36:09 +0100

    added JUnit test 5

---
+++ src/ErrorProcessorTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ErrorProcessorTest {
+
+    @Test
+    void subscribe() {
+    }
+
+    @Test
+    void onSubscribe() {
+    }
+
+    @Test
+    void onNext() {
+    }
+
+    @Test
+    void onError() {
+    }
+
+    @Test
+    void onComplete() {
+    }
+}
---
+++ src/LogSubscriberTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogSubscriberTest {
+
+    @Test
+    void onSubscribe() {
+    }
+
+    @Test
+    void onNext() {
+    }
+
+    @Test
+    void onError() {
+    }
+
+    @Test
+    void onComplete() {
+    }
+}
